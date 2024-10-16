244beac3d653ef7b801df6dc3440f58c36f9179e
JMette <jmette@uni-bremen.de>
2024-10-16 19:26:05 +0200

    added unfinished functions for background fit and delta calculation and several control plot sections

--- hyvent/develop.py
+++ hyvent/develop.py
@@ -16,7 +16,7 @@ from hyvent.io import (read_from_csv,
 from hyvent.plotting_maps import *
 from hyvent.plotting import *
 from hyvent.misc import keys_to_data
-from hyvent.processing import calc_mean_profile, derive_mapr, derive_CTD, subtract_bg
+from hyvent.processing import calc_mean_profile, derive_mapr, derive_CTD
 
 import pandas as pd
 import gsw
@@ -30,6 +30,10 @@ gebcopath = '/home/jonathan/Dokumente/SHK Maren/PS137/PS137_AuroraVent_25m_bilin
 gebcopath_hillshade = '/home/jonathan/Dokumente/SHK Maren/PS137/PS137_AuroraVent_25m_bilinear_WGS84_hillshade.nc'
 fig_path = '/home/jonathan/Dokumente/Masterarbeit/Thesis_Latex_Project/figures/4-results/obs/'
 
+lonW = -6.45
+lonE = -6.175
+latS = 82.875
+latN = 82.92
 vent_loc = (parse("6° 15.32'W"),parse("82° 53.83'N"))
 aurora_stations = ['022_01', '026_01', '028_01', '033_01', '036_01', '041_01', '054_01', '055_01']
 
@@ -79,31 +83,91 @@ plot2D_station(profile_data, 'PS137_036_01', 'dORP', vent_loc, 2000, 5000,bathy)
 plot_section(profile_data, 'PS137_028_01', 'CTD_lat', 'DEPTH', 'dORP', 2400, 20)
 plot_section(profile_mapr[profile_mapr['Station']=='028_01'], '', 'CTD_lat', 'density', 'dORP', 2400, 20)
 
+# %% substract background
+
+def get_bg_fit(bg, var, min_dep, max_dep, fit_order=10):
+    """
+    This funtions fits a polynomial to a variable of a profile (usually to create a smooth background profile), with depth as x values for the fit.
+
+    Parameters
+    ----------
+    bg : pandas dataframe
+        Dataset which should be used as background.
+    var : string
+        Variable, where the deviation should be calculated.
+    min_dep : int
+        Minimum depth for the fit's depth range.
+    max_dep : int
+        Maximum depth for the fit's depth range.
+    fit_order : TYPE, optional
+        Order of the polynomial fit. The default is 10.
+
+    Returns
+    -------
+    df_fit : pandas dataframe
+        Datframe containing the depth and the fit result.
+
+    """
+    import numpy as np
+    import pandas as pd
+    from hyvent.processing import sep_casts
+
+    #get first downcast of background and subset by depth range
+    bg = sep_casts(bg, window_size=5000)
+    if len(bg) > 2:
+        print('Warning: Your background station was separated into more than two up or down casts. Check the casts returned by sep_casts and the used window size.')
+    bg = bg[0]
+    bg = bg[(bg['DEPTH']>=min_dep) & (bg['DEPTH']<=max_dep)]
+
+    coef = np.polyfit(bg['DEPTH'],bg[var],fit_order)
+    fit = np.poly1d(coef)
+    fit = fit(bg['DEPTH'])
+    df = {'DEPTH':bg['DEPTH'],'Fit_'+var:fit}
+    df_fit = pd.DataFrame(df)
+
+    return df_fit
+
+
+def calc_delta_by_fit(data, bg, var, min_dep, max_dep): #casts: data, order: Order of PolyFit
+
+    bg_fit = get_bg_fit(bg, var, min_dep, max_dep)
+    pd.merge_asof(data[pd.notna(data['DEPTH'])], bg_fit, on='DEPTH', direction='nearest')
+    data['Delta_'+var] = data[var] - data['Fit_'+var]
+
+    return data
+
+delta_fit036 = calc_delta_by_fit(sta036, profile_background, 'potemperature', 2000, 5000)
+
+
+#%% test bg fit
+
+plt.plot(bg['potemperature'],bg['DEPTH'])
+plt.plot(Bg_fit['Bg_fit_potemperature'],Bg_fit['DEPTH'])
+
+
+#%% plot for anomaly
+
+sta036 = profile_data[profile_data['Station']=='036_01']
+#delta_dens036 = subtract_bg_by_press(sta036, profile_background, 'potemperature', 1800, 5000)
+delta_presslayers036 = calcTempAnomalyPress(sta036, profile_background, 100)
+
 #%%
-data = profile_data.copy(deep=True)
-var_list = ['CTD_lon','CTD_lat']
-window_size = 7
-order = 3
+plt.figure()
+plt.plot(sta036['potemperature'],sta036['DEPTH'],label='theta 036')
+plt.plot(profile_background['potemperature'],profile_background['DEPTH'],label='theta 018')
 
-from scipy.signal import savgol_filter
-from scipy.interpolate import UnivariateSpline
+plt.gca().invert_yaxis()
+plt.legend()
 
-data_list = [d for _, d in data.groupby(['Station','SN'])]
-for profile in data_list:
-    for var in var_list:
-        new_col = 'Fit_'+str(var)
-        profile[new_col] = savgol_filter(profile[var], window_size, order)
-    plt.plot(profile['CTD_lon'],profile['CTD_lat'],marker='x')
-    plt.plot(profile['Fit_CTD_lon'],profile['Fit_CTD_lat'])
+#%%
+plt.figure()
+plt.plot(delta036['TempAnomaly'],delta036['DEPTH'],label='delta theta')
+plt.plot(delta_presslayers036['TempAnomaly'],delta_presslayers036['DEPTH'],label='delta theta press')
+#plt.plot(delta_dens036['Delta_potemperature'],delta_dens036['DEPTH'],label='delta theta dens')
 
 
-
-#plot_track(profile_data, vent_loc=vent_loc, bathy=bathy)
-
-
-#%% slize bathy
-
-
+plt.gca().invert_yaxis()
+plt.legend()
 
 
 
