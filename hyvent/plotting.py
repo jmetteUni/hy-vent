a56c25821cb9e2412eb5454f98e16255acee32c8
JMette <jmette@uni-bremen.de>
2024-11-10 16:42:25 +0100

    added code for number of xticks in depth_plot

--- hyvent/plotting.py
+++ hyvent/plotting.py
@@ -425,10 +425,12 @@ def depth_plot(data,xvar,yvar,depth_min,background='None',path_save='None'):
             plt.plot(background[xvar],background[yvar],color='black',linewidth=1)
 
     if (xvar == 'Delta_potemperature') | (xvar == 'Delta_Sigma3') | (xvar == 'Delta_delta3He') | (xvar == 'Delta_Neph_outl(volts)'):
-        plt.axvline(0, color = 'black',alpha=0.5)
+        plt.axvline(0, color = 'black',alpha=0.3)
     plt.gca().invert_yaxis()
     plt.ylabel(get_var(yvar)[0])
     plt.xlabel(get_var(xvar)[0])
+
+    plt.locator_params(axis='x', nbins=8)
 
     if path_save != 'None':
         plt.savefig(path_save, dpi=300)
