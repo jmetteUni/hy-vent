547d12216032fa341a3527707ab2917ef73d1f7a
jmetteUni <j.mette@posteo.de>
2026-05-16 13:19:20 +0200

    added custom theme for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-#html_theme = "pydata_sphinx_theme"
+#preinstalled fallback theme
+#html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
